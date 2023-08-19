41e3a5b5659803fc39943f7000ab35499e1fa4bc
Avinash Pandey <Avinash.Ranjan21@gmail.com>
2023-08-19 15:29:10 +0530

    #LeetCode: Problem number 578 ContainDuplicates.kt

---
+++ src/main/kotlin/ProblemSets/LeetCode/ContainDuplicates.kt
@@ -0,0 +1,20 @@
+package ProblemSets.LeetCode
+
+/**
+ * https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/578/
+ *
+ * Given an integer array numbs, return true if any value appears at least twice in the array, and return false if every element is distinct.
+ */
+class ContainDuplicates {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        if (nums.isEmpty()) return false
+        val numSet = mutableSetOf<Int>()
+        nums.forEach {
+            if (numSet.contains(it)){
+                return true;
+            }
+            numSet.add(it)
+        }
+        return false
+    }
+}
---
+++ src/test/kotlin/ProblemSets/LeetCode/ContainDuplicatesTest.kt
@@ -0,0 +1,16 @@
+package ProblemSets.LeetCode
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class ContainDuplicatesTest {
+
+    @Test
+    fun containsDuplicate() {
+        val containDuplicates = ContainDuplicates()
+        Assertions.assertFalse(containDuplicates.containsDuplicate(intArrayOf(2,3,4,5)))
+        Assertions.assertTrue(containDuplicates.containsDuplicate(intArrayOf(2,3,4,5,2)))
+        Assertions.assertFalse(containDuplicates.containsDuplicate(intArrayOf()))
+        Assertions.assertTrue(containDuplicates.containsDuplicate(intArrayOf(1,1,1,1,1)))
+    }
+}
