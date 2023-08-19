470adffb4484c9dff4178d9281bbf632b2c2529d
Avinash Pandey <Avinash.Ranjan21@gmail.com>
2023-08-19 11:30:12 +0530

    #Kotlin: Basic kotlin Pair and triple demo added

---
+++ src/main/kotlin/LanguagePractice/easy/PairAndTriplet.kt
@@ -0,0 +1,19 @@
+package LanguagePractice.easy
+
+/**
+ * A Pair is a simple data class that holds two values together.
+ * It's created using the two extension function, which is available
+ * on any object. Here's a basic explanation
+ *
+ * A Triple is similar to a Pair, but it holds three values together.
+ */
+
+fun main() {
+    val fullName = Pair("Avinash", "Pandey")
+    val accountNumber = 123456789
+    val creditCardNumber = Triple("123456789", "12/12/20", "123")
+    val accountDetails = Triple(fullName, accountNumber, creditCardNumber)
+    println("Account holder first name is ${fullName.first} and last name is ${fullName.second}")
+    println("Account details of ${fullName.first} is account number is ${accountDetails.second}")
+    println("Credit card details of ${fullName.first} is below \n ${creditCardNumber.first} \n ${creditCardNumber.second} \n ${creditCardNumber.third}")
+}
