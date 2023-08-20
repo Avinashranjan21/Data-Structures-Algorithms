8ee2cfedf2f8156919949b90b667b92fbcdb489e
Avinash Pandey <Avinash.Ranjan21@gmail.com>
2023-08-20 12:12:22 +0530

    #Kotlin: New code added at ListDemo.kt

--- src/main/kotlin/language_practice/collection/ListDemo.kt
+++ src/main/kotlin/language_practice/collection/ListDemo.kt
@@ -6,8 +6,11 @@ fun main() {
     var mutableList = mutableListOf("A", "B", "C", "D")
     mutableList.add("E") // Data can be added or removed from mutable list
     mutableList.removeAt(0)
+    mutableList.remove("B")
     println("This is immutable list $mutableList")
     mutableList = imMutableList.toMutableList()
     mutableList.addAll(listOf("E", "F", "G", "H"))
     println("This is updated immutable list $mutableList")
+    val resultList = imMutableList + mutableList
+    println("The returned list is of type Immutable list $resultList")
 }
