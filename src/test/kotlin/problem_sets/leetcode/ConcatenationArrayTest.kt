916026192180735eae633c4d14b8d3e80a300451
Avinash Pandey <Avinash.Ranjan21@gmail.com>
2023-10-02 10:02:07 +0530

    Array matching changed to jupiter api

--- src/test/kotlin/problem_sets/leetcode/ConcatenationArrayTest.kt
+++ src/test/kotlin/problem_sets/leetcode/ConcatenationArrayTest.kt
@@ -1,5 +1,6 @@
 package problem_sets.leetcode
 
+import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -10,8 +11,6 @@ class ConcatenationArrayTest {
         val concatenationArray = ConcatenationArray()
         val expectedArray = intArrayOf(1,2,1,1,2,1)
         val resultArray = concatenationArray.getConcatenation(intArrayOf(1,2,1))
-        for (i in 0..expectedArray.lastIndex){
-            assertEquals(expectedArray[i], resultArray[i])
-        }
+        assertArrayEquals(expectedArray,resultArray)
     }
 }
